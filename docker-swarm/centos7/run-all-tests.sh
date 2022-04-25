cbcd9500f1c8e212ee73a5db0a48c6c3d01c5d01
Tor Egge <Tor.Egge@online.no>
2022-04-25 12:42:16 +0200

    Remove --service-init argument when running tests in docker swarm.

--- docker-swarm/centos7/run-all-tests.sh
+++ docker-swarm/centos7/run-all-tests.sh
@@ -10,4 +10,4 @@ SERVICE_RAMDISK="--service-ramdisk"
 SERVICE_RESERVE_MEMORY="--service-reserve-memory 9GB"
 TEST_SELECTION=""
 
-$HOME/git/system-test/bin/run-tests-on-swarm.sh -i $USER-vespanode-centos7 -n $NUMNODES $SERVICE_CONSTRAINT --service-init $SERVICE_RAMDISK $SERVICE_RESERVE_MEMORY --resultdir $(pwd)/systemtests-swarm -c -d 5 $TEST_SELECTION
+$HOME/git/system-test/bin/run-tests-on-swarm.sh -i $USER-vespanode-centos7 -n $NUMNODES $SERVICE_CONSTRAINT $SERVICE_RAMDISK $SERVICE_RESERVE_MEMORY --resultdir $(pwd)/systemtests-swarm -c -d 5 $TEST_SELECTION
