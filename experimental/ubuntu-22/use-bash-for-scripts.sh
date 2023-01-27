96084bc45cf8d6d901e24f4e084d034e552f966e
Arne Juul <arnej@yahooinc.com>
2023-01-27 14:04:46 +0100

    add experimental setup for Ubuntu 22.04 LTS "Jammy Jellyfish"

---
+++ experimental/ubuntu-22/use-bash-for-scripts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+oldhashbang='#!/bin/sh'
+newhashbang='#!/bin/bash'
+
+if [ -f $VESPA_HOME/libexec/vespa/common-env.sh ]; then
+	echo "Changing shell scripts in $VESPA_HOME"
+else
+	echo "Please set VESPA_HOME first"
+	exit 1
+fi
+
+find $VESPA_HOME -type f -size -20 -print | while read filename; do
+	firstline=$(head -n 1 < ${filename})
+	if [ "$firstline" = "$oldhashbang" ]; then
+		echo "Fixing ${filename}"
+		( echo "$newhashbang"; cat ${filename} ) > ${filename}.bash
+		chmod +x ${filename}.bash
+		mv ${filename}.bash ${filename}
+	fi
+done
