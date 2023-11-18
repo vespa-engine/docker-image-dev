9ba3ef61e7812ad1e129aaf9a236679693363c7e
Tor Egge <Tor.Egge@online.no>
2023-11-18 22:32:58 +0100

    Add docker swarm setup for AlmaLinux 8.

---
+++ docker-swarm/almalinux-8/disable-vespanode-host.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+node=$1
+NODELIST=$(docker node ls --format '{{.Hostname}}' | tr '\n' ' ')
+
+for checknode in $NODELIST; do
+    if test $checknode = $node
+    then
+	docker node update --label-rm enable-$USER-vespanode $node
+	exit 0
+    fi
+done
+echo "Bad node $node, not in list of known nodes: $NODELIST" 1>&2
+exit 1
---
+++ docker-swarm/almalinux-8/vespanode-common.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
+
+# Sample settings
+if test -d $HOME/volumes/vespa-dev-almalinux-8
+then
+    VESPA_DEV_VOLUME=$HOME/volumes/vespa-dev-almalinux-8
+else
+    VESPA_DEV_VOLUME=volume-vespa-dev-almalinux-8
+fi
+VESPANODE_IMAGE_SUFFIX=almalinux-8
+LOCAL_REGISTRY_PORT=5000
+TUNNELED_REGISTRY_PORT=5000
+LOCAL_REGISTRY=127.0.0.1:$LOCAL_REGISTRY_PORT
+TUNNELED_REGISTRY=127.0.0.1:$TUNNELED_REGISTRY_PORT
+SSH_REGISTRY_TUNNEL_ARG="-R 127.0.0.1:$TUNNELED_REGISTRY_PORT:127.0.0.1:$LOCAL_REGISTRY_PORT"
+
+# Pick up overrides
+for overrides_file in $HOME/.vespa-docker-image-dev-swarm-settings $HOME/.vespa-docker-image-dev-swarm-settings-$VESPANODE_IMAGE_SUFFIX
+do
+    test -x $overrides_file && . $overrides_file
+done
+true
