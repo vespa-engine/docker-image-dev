0e58f3fce8571a8154cecb42074451ec4107fd45
Arnstein Ressem <aressem@yahooinc.com>
2024-02-15 15:29:01 +0100

    Remove CentOS Stream 8 dev.

--- docker-swarm/centos-stream8/distribute-vespanode.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
-
-host=$1
-DOCKER_IMAGE=$2
-. $(dirname $0)/vespanode-common.sh
-ssh -a $SSH_REGISTRY_TUNNEL_ARG -o ExitOnForwardFailure=yes $host bash -lc "'git/docker-image-dev/docker-swarm/centos-stream8/download-vespanode.sh $DOCKER_IMAGE $TUNNELED_REGISTRY'"
