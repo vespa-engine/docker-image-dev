a679e6fbc95698fba8d3219dec1ba2d8f30ba6e3
Henning Baldersheim <balder@yahoo-inc.com>
2024-04-27 07:39:58 +0000

    Upgrade dependencies openssl-3.1.5, zstd-1.5.6, onnxruntime.1.17.3 openblas-0.3.27 and llama-3.0.1

--- build/almalinux-8/include/install-build-dependencies.sh
+++ build/almalinux-8/include/install-build-dependencies.sh
@@ -37,7 +37,7 @@ dnf -y install http://mirror.centos.org/centos/8-stream/AppStream/${mycpu}/os/Pa
 GIT_REPO="https://github.com/vespa-engine/vespa"
 
 # Change git reference for a specific version of the vespa.spec file. Use a tag or SHA to allow for reproducible builds.
-VESPA_SRC_REF="8db9ee454f4ae9c677fdf9382fcb51139fbc263d"
+VESPA_SRC_REF="5bb42bd70deb49b31cc79684a2be99aeea8e55da"
 
 # Fetch the RPM spec for vespa
 curl -Lf -O $GIT_REPO/raw/$VESPA_SRC_REF/dist/vespa.spec
