6d51f4dd28f359eebfa11c70f786eb12ea76bcf0
Arne Juul <arnej@vespa.ai>
2024-05-27 11:42:19 +0000

    bump for onnxruntime 1.18.0

--- build/almalinux-8/include/install-build-dependencies.sh
+++ build/almalinux-8/include/install-build-dependencies.sh
@@ -38,7 +38,7 @@ dnf -y install http://mirror.centos.org/centos/8-stream/AppStream/${mycpu}/os/Pa
 GIT_REPO="https://github.com/vespa-engine/vespa"
 
 # Change git reference for a specific version of the vespa.spec file. Use a tag or SHA to allow for reproducible builds.
-VESPA_SRC_REF="e5d7246b898c66c7b7f24a15eb8e9d9d793ae03e"
+VESPA_SRC_REF="388bb62b467c9dfd72521b02ad0491bd123d5afd"
 
 # Fetch the RPM spec for vespa
 curl -Lf -O $GIT_REPO/raw/$VESPA_SRC_REF/dist/vespa.spec
