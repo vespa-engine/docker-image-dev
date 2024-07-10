f83ce40cc7dcbe9ec2a967c3524a9bd078311019
Tor Egge <Tor.Egge@online.no>
2024-07-10 13:29:13 +0200

    Update testing on swarm documentation.

--- docker-swarm/almalinux-8/run-vespanode-registry.sh
+++ docker-swarm/almalinux-8/run-vespanode-registry.sh
@@ -12,4 +12,4 @@ CONTAINER_NAME=$USER-vespanode-registry
 VOLUME_NAME=${CONTAINER_NAME}
 docker container stop ${CONTAINER_NAME}
 docker container rm ${CONTAINER_NAME}
-docker run -d -p ${LOCAL_REGISTRY_PORT}:5000 -v ${VOLUME_NAME}:/var/lib/registry --name ${CONTAINER_NAME} registry:2
+docker run -d -p ${LOCAL_REGISTRY}:5000 -v ${VOLUME_NAME}:/var/lib/registry --name ${CONTAINER_NAME} registry:2
