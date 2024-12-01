301849f3b93e21b42e9b7bfccc53888be8c9027b
Arne Juul <arnej@vespa.ai>
2024-12-01 18:18:00 +0000

    bump SRC_REF for onnxruntime 1.20.1

--- build/almalinux-9/include/install-build-dependencies.sh
+++ build/almalinux-9/include/install-build-dependencies.sh
@@ -45,7 +45,7 @@ dnf -y install \
 GIT_REPO="https://github.com/vespa-engine/vespa"
 
 # Change git reference for a specific version of the vespa.spec file. Use a tag or SHA to allow for reproducible builds.
-VESPA_SRC_REF="ae3f3a5ad659da2e34490cb6a2dd648745ac8248"
+VESPA_SRC_REF="b0402dc7d475bc65ac85d5d989b915f28d343ab8"
 
 # Fetch the RPM spec for vespa
 curl -Lf -O $GIT_REPO/raw/$VESPA_SRC_REF/dist/vespa.spec
