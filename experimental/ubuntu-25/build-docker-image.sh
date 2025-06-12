4dce60700720985971c3dce22d3ea9b8df38ba48
Arne Juul <arnej@vespa.ai>
2025-06-12 13:45:41 +0200

    Dockerfiles for compiling Vespa on Ubuntu

---
+++ experimental/ubuntu-25/build-docker-image.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+UBUNTU_VERSION=25.04
+
+case $(uname -m) in
+	arm64|aarch64) set - "--build-arg" "ONNXCPU=aarch64" "$@" ;;
+esac
+
+myusername=$(id -un)
+set - "--build-arg" "MYUSERNAME=$myusername" "$@"
+
+echo BUILDING: docker build -t vespa-ubuntu-dev:${UBUNTU_VERSION} "$@" .
+docker build -t vespa-ubuntu-dev:${UBUNTU_VERSION} "$@" .
