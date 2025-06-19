cd1b0850f411ca7336f6e9f316d8c8d1ee5a7bcf
Arne Juul <arnej@vespa.ai>
2025-06-19 10:33:25 +0000

    bump jllama major version

--- build/almalinux-9/include/install-build-dependencies.sh
+++ build/almalinux-9/include/install-build-dependencies.sh
@@ -58,7 +58,7 @@ fi
 GIT_REPO="https://github.com/vespa-engine/vespa"
 
 # Change git reference for a specific version of the vespa.spec file. Use a tag or SHA to allow for reproducible builds.
-VESPA_SRC_REF="c6a66e6a3bf9982020c67d37498cf8cae59cb501"
+VESPA_SRC_REF="a2d08bfa2217a750a6ba90d8eabe9e9b83410d3c"
 
 # Fetch the RPM spec for vespa
 curl -Lf -O $GIT_REPO/raw/$VESPA_SRC_REF/dist/vespa.spec
