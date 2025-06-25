524cfad954f76e5c1d062debc5682aba8e9b8d6f
Tor Egge <Tor.Egge@online.no>
2025-06-25 12:26:29 +0200

    Use registry:3

--- docker-swarm/almalinux-8/run-vespanode-registry.sh
+++ docker-swarm/almalinux-8/run-vespanode-registry.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Copyright Vespa.ai. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-# docker pull registry:2
-if test "$(docker image ls  -f reference=registry:2 | wc -l)" -le 1
+# docker pull registry:3
+if test "$(docker image ls  -f reference=registry:3 | wc -l)" -le 1
 then
-    echo "registry:2 docker image is missing." 1>&2
+    echo "registry:3 docker image is missing." 1>&2
     exit 1
 fi
 . "$(dirname "$0")"/vespanode-common.sh
@@ -12,4 +12,4 @@ CONTAINER_NAME=$USER-vespanode-registry
 VOLUME_NAME=${CONTAINER_NAME}
 docker container stop "${CONTAINER_NAME}"
 docker container rm "${CONTAINER_NAME}"
-docker run -d -p "${LOCAL_REGISTRY}:5000" -v "${VOLUME_NAME}":/var/lib/registry --name "${CONTAINER_NAME}" registry:2
+docker run -d -p "${LOCAL_REGISTRY}:5000" -v "${VOLUME_NAME}":/var/lib/registry --name "${CONTAINER_NAME}" registry:3
