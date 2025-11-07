a227708935a718507a6e30aa0bf2d250024dc9a2
Esolitos (Marlon) <git@esolitos.com>
2025-11-07 07:39:16 +0100

    fix: use parameter expansion to safely check SSH env vars

--- build/almalinux-9/include/ssh-env-config.sh
+++ build/almalinux-9/include/ssh-env-config.sh
@@ -20,21 +20,21 @@ fi
 # The environment variables are unset after the files are created to help
 # prevent accidental output in logs
 
-if [ -z "$SSH_CONFIG" ] && \
-  [ -z "$SSH_CONFIG_B64" ] && \
-  [ -z "$SSH_CONFIG_PATH" ] && \
-  [ -z "$SSH_KNOWN_HOSTS" ] && \
-  [ -z "$SSH_KNOWN_HOSTS_B64" ] && \
-  [ -z "$SSH_KNOWN_HOSTS_PATH" ] && \
-  [ -z "$SSH_PRIVATE_RSA_KEY" ] && \
-  [ -z "$SSH_PRIVATE_RSA_KEY_B64" ] && \
-  [ -z "$SSH_PRIVATE_RSA_KEY_PATH" ] && \
-  [ -z "$SSH_PRIVATE_DSA_KEY" ] && \
-  [ -z "$SSH_PRIVATE_DSA_KEY_B64" ] && \
-  [ -z "$SSH_PRIVATE_DSA_KEY_PATH" ] && \
-  [ -z "$SSH_DEBUG" ]; then
+if [ -z "${SSH_CONFIG:-}" ] && \
+  [ -z "${SSH_CONFIG_B64:-}" ] && \
+  [ -z "${SSH_CONFIG_PATH:-}" ] && \
+  [ -z "${SSH_KNOWN_HOSTS:-}" ] && \
+  [ -z "${SSH_KNOWN_HOSTS_B64:-}" ] && \
+  [ -z "${SSH_KNOWN_HOSTS_PATH:-}" ] && \
+  [ -z "${SSH_PRIVATE_RSA_KEY:-}" ] && \
+  [ -z "${SSH_PRIVATE_RSA_KEY_B64:-}" ] && \
+  [ -z "${SSH_PRIVATE_RSA_KEY_PATH:-}" ] && \
+  [ -z "${SSH_PRIVATE_DSA_KEY:-}" ] && \
+  [ -z "${SSH_PRIVATE_DSA_KEY_B64:-}" ] && \
+  [ -z "${SSH_PRIVATE_DSA_KEY_PATH:-}" ] && \
+  [ -z "${SSH_DEBUG:-}" ]; then
     # none of the ENV vars we care about found, so skip the logic in this script
-    [[ $1 ]] && exec "$@"
+    [[ ${1:-} ]] && exec "$@"
 fi
 
 mkdir -p ~/.ssh
@@ -53,78 +53,78 @@ decode_base64() {
 
 ## ~/.ssh/config
 
-[[ -n "$SSH_CONFIG" ]] && \
+[[ -n "${SSH_CONFIG:-}" ]] && \
   echo "$SSH_CONFIG" > ~/.ssh/config && \
   chmod 600 ~/.ssh/config && \
   unset SSH_CONFIG
 
-[[ -n "$SSH_CONFIG_B64" ]] && \
+[[ -n "${SSH_CONFIG_B64:-}" ]] && \
   decode_base64 "$SSH_CONFIG_B64" > ~/.ssh/config && \
   chmod 600 ~/.ssh/config && \
   unset SSH_CONFIG_B64
 
-[[ -n "$SSH_CONFIG_PATH" && ! -a ~/.ssh/config ]] && \
+[[ -n "${SSH_CONFIG_PATH:-}" && ! -a ~/.ssh/config ]] && \
   cp "$SSH_CONFIG_PATH" ~/.ssh/config && \
   chmod 600 ~/.ssh/config && \
   unset SSH_CONFIG_PATH
 
 ## ~/.ssh/known_hosts
 
-[[ -n "$SSH_KNOWN_HOSTS" ]] && \
+[[ -n "${SSH_KNOWN_HOSTS:-}" ]] && \
   echo "$SSH_KNOWN_HOSTS" > ~/.ssh/known_hosts && \
   chmod 600 ~/.ssh/known_hosts && \
   unset SSH_KNOWN_HOSTS
 
-[[ -n "$SSH_KNOWN_HOSTS_B64" ]] && \
+[[ -n "${SSH_KNOWN_HOSTS_B64:-}" ]] && \
   decode_base64 "$SSH_KNOWN_HOSTS_B64" > ~/.ssh/known_hosts && \
   chmod 600 ~/.ssh/known_hosts && \
   unset SSH_KNOWN_HOSTS_B64
 
-[[ -n "$SSH_KNOWN_HOSTS_PATH" && ! -a ~/.ssh/known_hosts ]] && \
+[[ -n "${SSH_KNOWN_HOSTS_PATH:-}" && ! -a ~/.ssh/known_hosts ]] && \
   cp "$SSH_KNOWN_HOSTS_PATH" ~/.ssh/known_hosts && \
   chmod 600 ~/.ssh/known_hosts && \
   unset SSH_KNOWN_HOSTS_PATH
 
 ## ~/.ssh/id_rsa
 
-[[ -n "$SSH_PRIVATE_RSA_KEY" ]] && \
+[[ -n "${SSH_PRIVATE_RSA_KEY:-}" ]] && \
   echo "$SSH_PRIVATE_RSA_KEY" > ~/.ssh/id_rsa && \
   chmod 600 ~/.ssh/id_rsa && \
   unset SSH_PRIVATE_RSA_KEY
 
-[[ -n "$SSH_PRIVATE_RSA_KEY_B64" ]] && \
+[[ -n "${SSH_PRIVATE_RSA_KEY_B64:-}" ]] && \
   decode_base64 "$SSH_PRIVATE_RSA_KEY_B64" > ~/.ssh/id_rsa && \
   chmod 600 ~/.ssh/id_rsa && \
   unset SSH_PRIVATE_RSA_KEY_B64
 
-[[ -n "$SSH_PRIVATE_RSA_KEY_PATH" && ! -a ~/.ssh/id_rsa ]] && \
+[[ -n "${SSH_PRIVATE_RSA_KEY_PATH:-}" && ! -a ~/.ssh/id_rsa ]] && \
   cp "$SSH_PRIVATE_RSA_KEY_PATH" ~/.ssh/id_rsa && \
   chmod 600 ~/.ssh/id_rsa && \
   unset SSH_PRIVATE_RSA_KEY_PATH
 
 ## ~/.ssh/id_dsa
 
-[[ -n "$SSH_PRIVATE_DSA_KEY" ]] && \
+[[ -n "${SSH_PRIVATE_DSA_KEY:-}" ]] && \
   echo "$SSH_PRIVATE_DSA_KEY" > ~/.ssh/id_dsa && \
   chmod 600 ~/.ssh/id_dsa && \
   unset SSH_PRIVATE_DSA_KEY
 
-[[ -n "$SSH_PRIVATE_DSA_KEY_B64" ]] && \
+[[ -n "${SSH_PRIVATE_DSA_KEY_B64:-}" ]] && \
   decode_base64 "$SSH_PRIVATE_DSA_KEY_B64" > ~/.ssh/id_dsa && \
   chmod 600 ~/.ssh/id_dsa && \
   unset SSH_PRIVATE_DSA_KEY_B64
 
-[[ -n "$SSH_PRIVATE_DSA_KEY_PATH" && ! -a ~/.ssh/id_dsa ]] && \
+[[ -n "${SSH_PRIVATE_DSA_KEY_PATH:-}" && ! -a ~/.ssh/id_dsa ]] && \
   cp "$SSH_PRIVATE_DSA_KEY_PATH" ~/.ssh/id_dsa && \
   chmod 600 ~/.ssh/id_dsa && \
   unset SSH_PRIVATE_DSA_KEY_PATH
 
 ## ssh debug mode
 
-[[ -n "$SSH_DEBUG" ]] && \
+[[ -n "${SSH_DEBUG:-}" ]] && \
   touch ~/.ssh/config && \
   chmod 600 ~/.ssh/config && \
   echo -e "Host *\n  LogLevel DEBUG3" >> ~/.ssh/config && \
   unset SSH_DEBUG
 
-[[ $1 ]] && exec "$@"
+[[ ${1:-} ]] && exec "$@"
